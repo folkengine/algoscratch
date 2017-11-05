cea8c778428735d990e0daf3b74169f307bd521f
Christoph <ignatz@gmail.com>
2017-11-05 13:22:00 -0500

    Permutations pass.

--- src/main/java/com/folkengine/algoscratch/CharPurmutations.java
+++ src/main/java/com/folkengine/algoscratch/CharPurmutations.java
@@ -2,29 +2,77 @@ package com.folkengine.algoscratch;
 
 import com.google.common.base.Joiner;
 import com.google.common.primitives.Chars;
-import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.val;
 
 import java.util.*;
 
-@Data
+@EqualsAndHashCode
 public class CharPurmutations {
 
-    Set<Character> chars;
+    @Getter private final Set<Character> chars;
+    @Getter private Set<String> permutations = new HashSet<>();
 
+    /**
+     * Takes a String, strips out any whitespace, and converts it into a Set of chars.
+     * Then calculates all unique permutations of those chars.
+     *
+     * @param s
+     */
     public CharPurmutations(String s) {
         chars = stringToUniqueChars(s.replaceAll("\\s", ""));
+        permute();
     }
 
     public static Set<Character> stringToUniqueChars(String s) {
         return new HashSet<Character>(Chars.asList(s.trim().toCharArray()));
     }
 
-    public static String setToSortedString(Set<Character> characters) {
-        List<Character> charlist = new ArrayList<Character>(characters);
-        Collections.sort(new ArrayList<Character>(charlist));
-        return Joiner.on("").join(charlist);
+    public static List<Character> setToSortedList(Set<Character> characters) {
+        val charlist = new ArrayList<Character>(characters);
+        Collections.sort(charlist);
+        return charlist;
     }
 
+    public static String setToSortedString(Set<Character> characters) {
+        return Joiner.on("").join(setToSortedList(characters));
+    }
+
+    private void add(List<Character> clist) {
+        String perm = Joiner.on("").join(clist);
+        if (permutations.contains(perm)) {
+            System.out.println("   " + perm + " already exists");
+        }
+        System.out.println(String.format("   Adding %s", perm));
+        permutations.add(perm);
+    }
+
+    private void permute() {
+        List<Character> sortedChars = setToSortedList(chars);
+
+        permute(setToSortedList(chars), 0, chars.size() - 1);
+    }
+
+    private void permute(List<Character> clist, int left, int right) {
+        System.out.println(String.format("   permute(%s, %d, %d)", clist.toString(), left, right));
+        if (left == right) {
+            add(clist);
+        } else {
+            for (int x = 0; x <= right; x++) {
+                clist = swap(clist, left, x);
+                System.out.println(String.format("   >>> %d: swapping %s & %s %s, %d, %d", x, clist.get(left), clist.get(x), clist.toString(), left, right));
+                permute(clist, left + 1, right);
+                clist = swap(clist, left, x);
+                System.out.println(String.format("   >>> %d: SWAPPING %s & %s %s, %d, %d", x, clist.get(left), clist.get(x), clist.toString(), left, right));
+            }
+        }
+    }
+
+    private List<Character> swap(List<Character> clist, int i, int j) {
+        char temp = clist.get(i);
+        clist.set(i, clist.get(j));
+        clist.set(j, temp);
+        return clist;
+    }
 }
--- src/test/java/com/folkengine/algoscratch/CharPurmutationsTest.java
+++ src/test/java/com/folkengine/algoscratch/CharPurmutationsTest.java
@@ -2,19 +2,28 @@ package com.folkengine.algoscratch;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CharPurmutationsTest {
 
     @Test
+    void charPurmutations() {
+        assertEquals(Set.of('c', 'd', 'e'), new CharPurmutations(" d ee c c d c").getChars());
+    }
+
+    @Test
     void stringToUniqueChars() {
+        assertEquals(Set.of('a', 'b', 'c'), CharPurmutations.stringToUniqueChars("ccbcaabaabcbaaabb"));
+    }
 
-        Set<Character> chars = Set.of('a', 'b', 'c');
-
-        assertEquals(chars, CharPurmutations.stringToUniqueChars("ccbcaabaabcbaaabb"));
+    @Test
+    void setToSortedList() {
+        assertEquals(
+                List.of('A', 'B', 'D'),
+                CharPurmutations.setToSortedList(Set.of('D', 'A', 'B')));
     }
 
     @Test
@@ -23,4 +32,11 @@ class CharPurmutationsTest {
 
         assertEquals("ABC", CharPurmutations.setToSortedString(chars));
     }
+
+    @Test
+    void permutations() {
+        // assertEquals(Set.of("AB", "BA"), new CharPurmutations("BA").getPermutations());
+        assertEquals(Set.of("ABC", "ACB", "BAC", "BCA", "CBA", "CAB"), new CharPurmutations("ABC").getPermutations());
+    }
 }
+
