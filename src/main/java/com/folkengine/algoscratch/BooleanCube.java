a4253407ed2221144cdbd0ef50690d640c4b99c3
Christoph <ignatz@gmail.com>
2017-11-06 11:23:18 -0500

    Tabling BooleanCube

--- src/main/java/com/folkengine/algoscratch/BooleanCube.java
+++ src/main/java/com/folkengine/algoscratch/BooleanCube.java
@@ -2,13 +2,37 @@ package com.folkengine.algoscratch;
 
 import lombok.Getter;
 
+/**
+ * 1:
+ * [0][0][0] [0][1][0] [0][2][0]
+ * [1][0][0] [1][1][0] [1][2][0]
+ * [2][0][0] [2][1][0] [2][2][0]
+ *
+ * mid:
+ * [0][0][1] [0][1][1] [0][2][1]
+ * [1][0][1] [1][1][1] [1][2][1]
+ * [2][0][1] [2][1][1] [2][2][1]
+ *
+ * 6:
+ * [0][0][2] [0][1][2] [0][2][2]
+ * [1][0][2] [1][1][2] [1][2][2]
+ * [2][0][2] [2][1][2] [2][2][2]
+ */
 public class BooleanCube {
 
     @Getter private final int size;
+    @Getter private boolean cube[][][];
 
     public BooleanCube(int size) {
         this.size = size;
+        cube = new boolean[size][size][size];
     }
 
+    @Override
+    public String toString() {
+        StringBuffer sb = new StringBuffer();
+        
+        return "";
+    }
 
 }
--- src/test/java/com/folkengine/algoscratch/BooleanCubeTest.java
+++ src/test/java/com/folkengine/algoscratch/BooleanCubeTest.java
@@ -1,14 +1,22 @@
 package com.folkengine.algoscratch;
 
+import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@Log4j2
 class BooleanCubeTest {
 
     @Test
     public void booleanCube() {
         assertEquals(3, new BooleanCube(3).getSize());
+    }
+
+    @Test
+    public void myToString() {
+        log.debug(new BooleanCube(4).toString());
+        assert(true);
     }
 
 }
